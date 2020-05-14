b05ef7c096d80d9127d6e1eb8d456a69f193f1bf
haruka.nakasaka <haruka128050@gmail.com>
2020-05-14 23:45:19 +0000

    Procfile

--- database/migrations/2020_05_12_080756_add_status_to_tasks_table.php
+++ database/migrations/2020_05_12_080756_add_status_to_tasks_table.php
@@ -14,7 +14,7 @@ class AddStatusToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('status')->nullable(false);
+            $table->string('status',10)->nullable(false);
         });
     }
 
@@ -26,7 +26,7 @@ class AddStatusToTasksTable extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->dropColumn('status')->nullable(false);
+            $table->dropColumn('status');
         });
     }
 }
